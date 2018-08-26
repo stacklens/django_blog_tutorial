d1ad598cd02bb5f5872ca8c9bb56a07ab17aff4c
dusai <474377974@qq.com>
2018-08-26 17:09:45 +0800

    write article model

---
+++ my_blog/urls.py
@@ -0,0 +1,26 @@
+"""my_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+# 记得引入include
+from django.urls import path, include
+
+# 存放了映射关系的列表
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # 新增代码，配置app的url
+    path('article/', include('article.urls', namespace='article')),
+]
