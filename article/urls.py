ee3788d14e278d5aa4c2697fa5a2ce3246054996
dusai <474377974@qq.com>
2018-09-16 16:46:43 +0800

    增加文章详情功能

--- article/urls.py
+++ article/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'article'
 
 urlpatterns = [
-    # path函数将url映射到视图
+    # 文章列表
     path('article-list/', views.article_list, name='article_list'),
+    # 文章详情
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
 ]
--- article/views.py
+++ article/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # 导入数据模型ArticlePost
 from .models import ArticlePost
 
+# 文章列表
 def article_list(request):
 	# 取出所有博客文章
 	articles = ArticlePost.objects.all()
@@ -10,3 +11,12 @@ def article_list(request):
 	context = { 'articles': articles }
 	# render函数：载入模板，并返回context对象
 	return render(request, 'article/list.html', context)
+
+# 文章详情
+def article_detail(request, id):
+	# 取出相应的文章
+	article = ArticlePost.objects.get(id=id)
+	# 需要传递给模板的对象
+	context = { 'article': article }
+	# 载入模板，并返回context对象
+	return render(request, 'article/detail.html', context)
