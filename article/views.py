8e4fda695767a4cce9ed32cab2e978194ff63d07
dusai <474377974@qq.com>
2018-09-18 21:44:09 +0800

    增加Markdown

--- article/views.py
+++ article/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
 # 导入数据模型ArticlePost
 from .models import ArticlePost
+
+# 引入markdown模块
+import markdown
 
 # 文章列表
 def article_list(request):
@@ -16,6 +18,16 @@ def article_list(request):
 def article_detail(request, id):
 	# 取出相应的文章
 	article = ArticlePost.objects.get(id=id)
+
+	# 将markdown语法渲染成html样式
+	article.body = markdown.markdown(article.body,
+		extensions=[
+		# 包含 缩写、表格等常用扩展
+		'markdown.extensions.extra',
+		# 语法高亮扩展
+		'markdown.extensions.codehilite',
+		])
+
 	# 需要传递给模板的对象
 	context = { 'article': article }
 	# 载入模板，并返回context对象
