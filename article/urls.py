8224a655566a696d02a3aef0c744ec19135dc981
dusai <474377974@qq.com>
2018-10-20 22:22:51 +0800

    编写修改文章功能

--- article/urls.py
+++ article/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
     # 写文章
     path('article-create/', views.article_create, name='article_create'),
+    # 删除文章
+    path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    # 更新文章
+    path('article-update/<int:id>/', views.article_update, name='article_update'),
 ]
--- article/views.py
+++ article/views.py
@@ -67,3 +67,50 @@ def article_create(request):
         context = { 'article_post_form': article_post_form }
         # 返回模板
         return render(request, 'article/create.html', context)
+
+
+# 删除文章
+def article_delete(request, id):
+    # 根据 id 获取需要删除的文章
+    article = ArticlePost.objects.get(id=id)
+    # 调用.delete()方法删除文章
+    article.delete()
+    # 完成删除后返回文章列表
+    return redirect("article:article_list")
+
+
+# 更新文章
+def article_update(request, id):
+    """
+    更新文章的视图函数
+    通过POST方法提交表单，更新titile、body字段
+    GET方法进入初始表单页面
+    id： 文章的 id
+    """
+
+    # 获取需要修改的具体文章对象
+    article = ArticlePost.objects.get(id=id)
+    # 判断用户是否为 POST 提交表单数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        article_post_form = ArticlePostForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存新写入的 title、body 数据并保存
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            article.save()
+            # 完成后返回到修改后的文章中。需传入文章的 id 值
+            return redirect("article:article_detail", id=id)
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+
+    # 如果用户 GET 请求获取数据
+    else:
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文，将 article 文章对象也传递进去，以便提取旧的内容
+        context = { 'article': article, 'article_post_form': article_post_form }
+        # 将响应返回到模板中
+        return render(request, 'article/update.html', context)
